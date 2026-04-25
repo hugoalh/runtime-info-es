f8589713b7f504d01db697e6f455c4b752645b58
hugoalh <hugoalh@gmail.com>
2026-04-25 17:24:58 +0800

    Update resources

---
+++ .npm-github.build.ts
@@ -0,0 +1,40 @@
+import { invokeDenoNodeJSTransformer } from "DNT";
+import { parse as parseJSONC } from "STD_JSONC";
+const jsrManifest = parseJSONC(await Deno.readTextFile("./jsr.jsonc"));
+await invokeDenoNodeJSTransformer({
+	copyEntries: [
+		"LICENSE.md",
+		"README.md"
+	],
+	//@ts-ignore Lazy type.
+	entrypointsScript: jsrManifest.exports,
+	generateDeclarationMap: true,
+	metadata: {
+		//@ts-ignore Lazy type.
+		name: jsrManifest.name,
+		//@ts-ignore Lazy type.
+		version: jsrManifest.version,
+		description: "A module for runtime information.",
+		keywords: [
+			"info",
+			"information",
+			"runtime"
+		],
+		homepage: "https://github.com/hugoalh/runtime-info-es#readme",
+		bugs: {
+			url: "https://github.com/hugoalh/runtime-info-es/issues"
+		},
+		license: "MIT",
+		author: "hugoalh",
+		repository: {
+			type: "git",
+			url: "git+https://github.com/hugoalh/runtime-info-es.git"
+		},
+		private: false,
+		publishConfig: {
+			access: "public"
+		}
+	},
+	outputDirectory: "dist/npm-github",
+	outputDirectoryPreEmpty: true
+});
